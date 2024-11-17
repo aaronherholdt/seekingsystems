75eb0ea286c7dde53c6d9c618f0bdf33f63d0aa2
aaronherholdt <121667179+aaronherholdt@users.noreply.github.com>
2024-11-17 15:13:36 +0200

    Update game.js

--- game.js
+++ game.js
@@ -10,12 +10,6 @@ socket.on("connect", () => {
     socket.emit("newPlayer", { id: socket.id, name: playerName });
 });
 
-// Listen for updates to the player list
-socket.on("playerList", (updatedPlayers) => {
-    players = updatedPlayers; // Update the players array
-    updatePlayersUI(); // Update the UI
-});
-
 // Phaser configuration
 const config = {
   type: Phaser.AUTO,
@@ -60,6 +54,12 @@ function updatePlayersUI() {
         .join("\n");
     playerListText.setText(`Players:\n${playerListContent}`);
 }
+
+// Listen for updates to the player list
+socket.on("playerList", (updatedPlayers) => {
+    players = updatedPlayers; // Update the players array
+    updatePlayersUI(); // Update the UI
+});
 
 function preload() {
     // Optional: Load any assets here
